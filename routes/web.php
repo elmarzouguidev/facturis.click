8012ef32055cb15bba705e0f98674c25e47f18ef
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-12-05 11:56:23 +0100

    add some configurations to the dashboard  v-3

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,14 @@
 use App\Http\Controllers\Facturis\Track\TrackerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
+Route::redirect('/','https://facturis.ma/');
 
-//Route::get('/track', [TrackerController::class, 'index'])->name('tracker');
 
 Route::get('/go/{shortURLKey}', '\AshAllenDesign\ShortURL\Controllers\ShortURLController');
-
 
 Route::group(['prefix' => config('facturis.admin_url')], function () {
     Voyager::routes();
